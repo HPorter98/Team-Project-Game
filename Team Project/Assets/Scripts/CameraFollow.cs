13d0813d80077629b4f45ad47cfa3ae0d5827d40
HPorter98 <78492245+HPorter98@users.noreply.github.com>
2021-02-13 23:20:39 +0000

    Added first iteration of movement
    Added the first iteration of player movement.

---
+++ Team Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    void FixedUpdate()
+    {//Change camera position to follow the player
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
---
+++ Team Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float sprintSpeed;
+    [SerializeField] private float cooldownTime;
+    private float nextUseTime = 0.0f;
+    public Rigidbody2D rd2d;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown("left shift"))
+        {//Increase speed for a Sprint
+            movementSpeed *= 1.5f;
+        }
+        else if (Input.GetKeyUp("left shift"))
+        {//Decrease speed back to a walk
+            movementSpeed /= 1.5f;
+        }
+        else if (Input.GetKeyDown("space"))
+        {//Press space for a dash
+            if (Time.time > nextUseTime)
+            {//If it is not on cooldown
+                if (movement.x == 1)
+                {
+                    rd2d.position = new Vector3(transform.position.x + 5.0f, transform.position.y + 0);
+                    nextUseTime = Time.time + cooldownTime;
+                }
+                else if (movement.x == -1)
+                {
+                    rd2d.position = new Vector3(transform.position.x + -5.0f, transform.position.y + 0);
+                    nextUseTime = Time.time + cooldownTime;
+                }
+                Debug.Log("Cooldown started");
+            }
+            else if (Time.time < nextUseTime)
+            {//If it is on cooldown
+                Debug.Log("Cooldown active");
+            }
+        }
+    }
+    void FixedUpdate()
+    {//move the character
+        rd2d.MovePosition(rd2d.position + movement.normalized * movementSpeed * Time.deltaTime);
+    }
+}
